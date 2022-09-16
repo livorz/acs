ca36fd8b0d710f1e04aada211511f729542959e8
Xeril-Lala <75497019+Xeril-Lala@users.noreply.github.com>
2022-09-15 18:18:15 -0700

    First commit on lala's branch
    +DaisyUI
    +Tailwind Config for themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        SBlue: "#0091cf",
+        SCBlue: "#67c1e0" 
+      }
+    },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+                  "primary": "#eeeeee",
+                  "secondary": "#661228",
+                  "accent": "#a8764a",
+                  "neutral": "#983433",
+                  "base-100": "#ffffff",
+                  "info": "#3ABFF8",
+                  "success": "#36D399",
+                  "warning": "#FBBD23",
+                  "error": "#F87272",
+                  },
+                },
+              ],
+            },
+  plugins: [require("daisyui")],
 }
