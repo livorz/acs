1dc04a8e6ef61b611081c10876915017d05755e5
Xeril-Lala <75497019+Xeril-Lala@users.noreply.github.com>
2022-09-21 21:50:43 -0700

    laptop commit 9212020 night

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -7,18 +7,20 @@ import "../dashboard/DashboardStyle.css";
     const Dashboard = () =>{
 
     return(
-
-<div class="grid overflow-hidden grid-lines grid-cols-5 auto-rows-auto gap-4">
-	<div class="box row-start-1 row-end-1">1</div>
-	<div class="box row-start-1 row-end-1">2</div>
-	<div class="box row-start-1 row-end-1">3</div>
-	<div class="box row-start-1 row-end-1">4</div>
-	<div class="box row-start-1 row-end-1 col-end-auto">5</div>
-	<div class="box row-start-2 row-end-4 col-start-1 col-end-4">6</div>
-	<div class="box row-start-2 row-end-6 col-start-4 col-end-6">7</div>
-	<div class="box row-start-4 row-end-6 col-start-1 col-end-4">8</div>
-</div>
-
+		<div className="grid overflow-hidden grid-lines grid-cols-5 auto-rows-auto gap-4">
+			<div className="box row-start-1 row-end-1">1
+				<div className="kpi-icon">
+					
+				</div>
+			</div>
+			<div className="box row-start-1 row-end-1">2</div>
+			<div className="box row-start-1 row-end-1">3</div>
+			<div className="box row-start-1 row-end-1">4</div>
+			<div className="box row-start-1 row-end-1 col-end-auto">5</div>
+			<div className="box row-start-2 row-end-4 col-start-1 col-end-4">6</div>
+			<div className="box row-start-2 row-end-6 col-start-4 col-end-6">7</div>
+			<div className="box row-start-4 row-end-6 col-start-1 col-end-4">8</div>
+		</div>
     );
 }
 
