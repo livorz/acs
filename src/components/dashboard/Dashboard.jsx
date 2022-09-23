0d74ea161e42bf94b50695aaeebf019fffc530f1
Xeril-Lala <75497019+Xeril-Lala@users.noreply.github.com>
2022-09-22 18:55:58 -0700

    laptop commit 9222022

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
-import React,{Fragment, useState, useEffect} from "react";
+import React,{Fragment, useState, useEffect} from "react"; 
+import { BsGenderFemale, BsGenderMale } from "react-icons/bs";
 //Components
 import StudentInfo from "./StudentInfo";
 //Styles
@@ -7,19 +8,21 @@ import "../dashboard/DashboardStyle.css";
     const Dashboard = () =>{
 
     return(
-		<div className="grid overflow-hidden grid-lines grid-cols-5 auto-rows-auto gap-4">
-			<div className="box row-start-1 row-end-1">1
-				<div className="kpi-icon">
-					
+		<div className="grid overflow-hidden grid-lines grid-cols-5 auto-rows-auto gap-2 mt-12 mx-12">
+			<div className="box row-start-1 row-end-1">
+				Genero
+				<BsGenderFemale className="bg-blue-600 text-white "/>
+				<BsGenderMale className="bg-fuchsia-500 text-white "/>
+
 				</div>
-			</div>
-			<div className="box row-start-1 row-end-1">2</div>
-			<div className="box row-start-1 row-end-1">3</div>
-			<div className="box row-start-1 row-end-1">4</div>
-			<div className="box row-start-1 row-end-1 col-end-auto">5</div>
-			<div className="box row-start-2 row-end-4 col-start-1 col-end-4">6</div>
-			<div className="box row-start-2 row-end-6 col-start-4 col-end-6">7</div>
-			<div className="box row-start-4 row-end-6 col-start-1 col-end-4">8</div>
+
+			<div className="box row-start-1 row-end-1">Estado Civil</div>
+			<div className="box row-start-1 row-end-1">Trabaja</div>
+			<div className="box row-start-1 row-end-1">Promedio TSU</div>
+			<div className="box row-start-1 row-end-1 col-end-auto">Promedio Ing</div>
+			<div className="box row-start-2 row-end-4 col-start-1 col-end-4">Vive con & Ingresos Familiares</div>
+			<div className="box row-start-2 row-end-6 col-start-4 col-end-6">Trabajo Razon & Trabajo Relacionado</div>
+			<div className="box row-start-4 row-end-6 col-start-1 col-end-4">Bachilleratos</div>
 		</div>
     );
 }
